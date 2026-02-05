4612d6d16258c71b2e81295cee4f1a9f2ad803c4
daikang6 <165463455+daikang6@users.noreply.github.com>
2026-02-05 11:40:22 +0800

    [ci] feat: add three npu workflow yml test (#4978)
    ### What does this PR do?
    
    Add npu e2e_one_step_off_policy_ascend
    、e2e_ppo_trainer_megatron_vllm_2_ascend、e2e_transferqueue_ascend
    workflow.
    
    ### Checklist Before Starting
    
    - [ ] Search for similar PRs. Paste at least one query link here: ...
    - [ ] Format the PR title as `[{modules}] {type}: {description}` (This
    will be checked by the CI)
    - `{modules}` include `fsdp`, `megatron`, `veomni`, `sglang`, `vllm`,
    `rollout`, `trainer`, `ci`, `training_utils`, `recipe`, `hardware`,
    `deployment`, `ray`, `worker`, `single_controller`, `misc`, `perf`,
    `model`, `algo`, `env`, `tool`, `ckpt`, `doc`, `data`, `cfg`, `reward`
    - If this PR involves multiple modules, separate them with `,` like
    `[megatron, fsdp, doc]`
      - `{type}` is in `feat`, `fix`, `refactor`, `chore`, `test`
    - If this PR breaks any API (CLI arguments, config, function signature,
    etc.), add `[BREAKING]` to the beginning of the title.
      - Example: `[BREAKING][fsdp, megatron] feat: dynamic batching`
    
    ### Test
    
    > For changes that can not be tested by CI (e.g., algorithm
    implementation, new model support), validate by experiment(s) and show
    results like training curve plots, evaluation results, etc.
    
    ### API and Usage Example
    
    > Demonstrate how the API changes if any, and provide usage example(s)
    if possible.
    
    ```python
    # Add code snippet or script demonstrating how to use this
    ```
    
    ### Design & Code Changes
    
    > Demonstrate the high-level design if this PR is complex, and list the
    specific changes.
    
    ### Checklist Before Submitting
    
    > [!IMPORTANT]
    > Please check all the following items before requesting a review,
    otherwise the reviewer might deprioritize this PR for review.
    
    - [ ] Read the [Contribute
    Guide](https://github.com/volcengine/verl/blob/main/CONTRIBUTING.md).
    - [ ] Apply [pre-commit
    checks](https://github.com/volcengine/verl/blob/main/CONTRIBUTING.md#code-linting-and-formatting):
    `pre-commit install && pre-commit run --all-files --show-diff-on-failure
    --color=always`
    - [ ] Add / Update [the
    documentation](https://github.com/volcengine/verl/tree/main/docs).
    - [ ] Add unit or end-to-end test(s) to [the CI
    workflow](https://github.com/volcengine/verl/tree/main/.github/workflows)
    to cover all the code. If not feasible, explain why: ...
    - [ ] Once your PR is ready for CI, send a message in [the `ci-request`
    channel](https://verl-project.slack.com/archives/C091TCESWB1) in [the
    `verl` Slack
    workspace](https://join.slack.com/t/verl-project/shared_invite/zt-3855yhg8g-CTkqXu~hKojPCmo7k_yXTQ).
    (If not accessible, please try [the Feishu group
    (飞书群)](https://applink.larkoffice.com/client/chat/chatter/add_by_link?link_token=772jd4f1-cd91-441e-a820-498c6614126a).)
    - [ ] If your PR is related to the `recipe` submodule, please also
    update the reference to the submodule commit via `git submodule update
    --remote` or `cd recipe && git pull origin main`.

--- tests/special_e2e/run_one_step_off_policy.sh
+++ tests/special_e2e/run_one_step_off_policy.sh
@@ -112,6 +112,13 @@ common_params=(
 
 )
 
+    # Detect device
+    device_name=$(python3 - <<'EOF'
+from verl.utils.device import get_device_name
+print(get_device_name())
+EOF
+)
+
 if [ "${ACTOR_STRATEGY}" == "fsdp2" ]; then
     echo "Running with FSDP2 strategy..."
     # FSDP2 specific parameters
@@ -148,6 +155,15 @@ elif [ "${ACTOR_STRATEGY}" == "megatron" ]; then
     ref_offload=True
     actor_offload=False
 
+    extra_flash_args=()
+
+    if [ "$device_name" == "npu" ]; then
+        echo "Detect NPU device, enabling FlashAttention..."
+        extra_flash_args+=(
+            ++actor_rollout_ref.actor.megatron.override_transformer_config.use_flash_attn=True
+        )
+    fi
+
     python3 -m verl.experimental.one_step_off_policy.main_ppo \
         --config-path=config \
         --config-name='one_step_off_ppo_megatron_trainer.yaml' \
@@ -165,6 +181,7 @@ elif [ "${ACTOR_STRATEGY}" == "megatron" ]; then
         actor_rollout_ref.rollout.tensor_model_parallel_size=${gen_tp} \
         actor_rollout_ref.ref.megatron.pipeline_model_parallel_size=${train_pp} \
         actor_rollout_ref.ref.megatron.tensor_model_parallel_size=${train_tp} \
+        "${extra_flash_args[@]}" \
         actor_rollout_ref.ref.megatron.param_offload=${ref_offload} $@
 else
     echo "Error: Unknown strategy ${ACTOR_STRATEGY}. Please use 'fsdp2' or 'megatron'"
